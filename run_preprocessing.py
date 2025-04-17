cbecd9ecc50073225015aa42d950d44e581aa113
Niklas Schmidt <bq_nschmidt@login1.bioquant.uni-heidelberg.de>
2025-04-17 10:51:54 +0200

    Working preprocessing

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -6,7 +6,7 @@ from module.data_processing import load_mri_data_2D, load_mri_data_2D_all_atlase
 
 
 config = Config(
-    RAW_DATA_DIR = "/net/data.isilon/ag-cherrmann/nschmidt/project/parse_xml_for_VAE/testing_files",  #"/net/data.isilon/ag-cherrmann/stumrani/mri_prep", 
+    RAW_DATA_DIR = "/net/data.isilon/ag-cherrmann/stumrani/mri_prep",  # "/net/data.isilon/ag-cherrmann/nschmidt/project/parse_xml_for_VAE/testing_files",  
     EXTRACTED_CSV_DIR = "/net/data.isilon/ag-cherrmann/nschmidt/project/parse_xml_for_VAE/xml_data",
     EXTRACTED_CSV_T_DIR = "/net/data.isilon/ag-cherrmann/nschmidt/project/parse_xml_for_VAE/xml_data_t",
     PROCESSED_CSV_DIR = "/net/data.isilon/ag-cherrmann/nschmidt/project/parse_xml_for_VAE/processed_data",
@@ -23,22 +23,23 @@ def get_all_data(directory: str, ext: str = "h5") -> list:
 
 
 valid_patients = valid_patients(config.METADATA_PATHS)
+
 process_all_paths(directory=config.RAW_DATA_DIR,
                   valid_patients=valid_patients,
-                  batch_size=5)
+                  batch_size=10)
 
 # Convert data from per atlas to per patient, either with all atlases or only using one. 
-if config.ALL_ATLASES is False: 
-    subjects_all, data_overview = load_mri_data_2D(data_path="./xml_data/Aggregated_suit.h5",  # Here a concrete example, but this can be anything.
-                                                   csv_paths=config.METADATA_PATHS,
-                                                   hdf5=True)
+# if config.ALL_ATLASES is False: 
+#     subjects_all, data_overview = load_mri_data_2D(data_path="./xml_data/Aggregated_suit.h5",  # Here a concrete example, but this can be anything.
+#                                                    csv_paths=config.METADATA_PATHS,
+#                                                    hdf5=True)
 
-elif config.ALL_ATLASES is True: 
-    data_paths = get_all_data(directory=config.EXTRACTED_CSV_DIR, ext="h5")
-    subjects_all, data_overview = load_mri_data_2D_all_atlases(data_paths=data_paths,
-                                                               csv_paths=config.METADATA_PATHS,
-                                                               hdf5=True)
+# elif config.ALL_ATLASES is True: 
+#     data_paths = get_all_data(directory=config.EXTRACTED_CSV_DIR, ext="h5")
+#     subjects_all, data_overview = load_mri_data_2D_all_atlases(data_paths=data_paths,
+#                                                                csv_paths=config.METADATA_PATHS,
+#                                                                hdf5=True)
     
-else: 
-    print("Please specify if all atlases should be processed, or only one.")
+# else: 
+#     print("Please specify if all atlases should be processed, or only one.")
                                                     
